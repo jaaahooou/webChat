62886a57b9828e6f46ce5a23dc17fc3e2f63093e
Jacek Pilch <jaaahooou@wp.pl>
2023-01-24 19:27:34 +0100

    redis

--- channelsChat/channelsChat/settings.py
+++ channelsChat/channelsChat/settings.py
@@ -126,3 +126,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = "channelsChat.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
